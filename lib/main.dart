e5c1a7cd9a1838ffa5938f3abb7cd33255a1fbea
Thiago Pinho <thiagopinho05@gmail.com>
2024-07-08 13:19:56 -0300

    🎉  Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    checkKey();
+    super.initState();
+  }
+
+  Future checkKey() async {
+    await dotenv.load(fileName: ".env");
+    String apiKey = dotenv.env['API_KEY'] ?? '';
+    log(apiKey);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: FlutterMap(
+        options: const MapOptions(
+          initialCenter: LatLng(-23.494280230442463, -46.41490418550601),
+          initialZoom: 16,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+          ),
+        ],
+      ),
+    );
+  }
+}
