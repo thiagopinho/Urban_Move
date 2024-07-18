86080b8397ec38177215f4933daf0ceca7ec67bc
Thiago Pinho <thiagopinho05@gmail.com>
2024-07-18 18:14:18 -0300

    chore: added new endpoint to get vehicles

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 class Endpoints {
   static const String buscarLinha = '/Linha/Buscar';
   static const String login = '/Login/Autenticar';
+  static const String getPositionVehicles = '/Posicao/Linha/Buscar';
 }
